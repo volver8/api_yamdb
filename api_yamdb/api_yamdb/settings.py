6f98238ebd831be83e7fb6aecb8f00366da9b58d
Rusakov Vladimir <rvs1998@yandex.ru>
2024-07-14 07:24:24 +0300

    update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.simplejwt',
+    'rest_framework_simplejwt',
     'django_filters',
     'users.apps.UsersConfig',
     'reviews.apps.ReviewsConfig',
