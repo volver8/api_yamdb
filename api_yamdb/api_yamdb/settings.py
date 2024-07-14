5d42ba075a52ced8ea487032e2ba309139f2f562
Evgeny Cherednichenko <More1ggg@yandex.ru>
2024-07-14 11:05:44 +0300

    Remove simple jwt from installed_apps

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'django_filters',
     'users.apps.UsersConfig',
     'reviews.apps.ReviewsConfig',
