8ff8815d180fdc93e4a1da2f4c4a4dea6f1881c3
Rusakov Vladimir <rvs1998@yandex.ru>
2024-07-14 07:10:47 +0300

    update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
+    'djoser',
     'django_filters',
     'users.apps.UsersConfig',
     'reviews.apps.ReviewsConfig',
